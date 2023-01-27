3dc700158217c8dddb0c640d558c2f8d19facae8
Butterscotch! <bscotchvanilla@gmail.com>
2023-01-27 18:23:38 -0500

    Add ratelimiting

--- BingusApi/Program.cs
+++ BingusApi/Program.cs
@@ -1,13 +1,35 @@
+using AspNetCoreRateLimit;
 using BingusApi;
 using BingusLib.FaqHandling;
 using BingusLib.HNSW;
+using MathNet.Numerics;
 using MessagePack;
 using MessagePack.Resolvers;
+using Microsoft.Extensions.Configuration;
 using RocksDbSharp;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add dependencies.
+// Load appsettings.json
+builder.Services.AddOptions();
+
+// Set up ratelimiting
+// Needed to store rate limit counters and ip rules
+builder.Services.AddMemoryCache();
+
+// Load general configuration from appsettings.json
+builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
+
+// Load IP rules from appsettings.json
+builder.Services.Configure<IpRateLimitPolicies>(builder.Configuration.GetSection("IpRateLimitPolicies"));
+
+// Inject counter and rules stores
+builder.Services.AddInMemoryRateLimiting();
+
+// configuration (resolvers, counter key builders)
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+
+// Add dependencies
 builder.Services.AddSingleton<IEmbeddingCache>(sp => new RocksDbCache(RocksDb.Open(new DbOptions().SetCreateIfMissing(true), "embedding_cache")));
 builder.Services.AddSingleton(sp => FaqConfigUtils.InitializeConfig(sp.GetService<ILogger<FaqConfig>>()));
 
@@ -32,7 +54,7 @@ builder.Services.AddSingleton(sp =>
     return faqHandler;
 });
 
-// Add services to the container.
+// Add services to the container
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -41,18 +63,19 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseIpRateLimiting();
 // app.UseHttpsRedirection();
 // app.UseAuthorization();
 app.MapControllers();
 
-// Service warmup.
+// Service warmup
 Task.Run(app.Services.GetService<FaqHandler>);
 
 app.Run();
