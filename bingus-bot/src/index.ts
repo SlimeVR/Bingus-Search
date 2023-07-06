f98b9ef92d28ad4e3137529a37053d0cf8c3c0c7
ImUrX <urielfontan2002@gmail.com>
2023-07-06 04:46:09 -0300

    add thread checking

--- bingus-bot/src/index.ts
+++ bingus-bot/src/index.ts
@@ -1,26 +1,25 @@
 import {
-  APIEmbed,
   Client,
+  EmbedBuilder,
   GatewayIntentBits,
-  JSONEncodable,
   REST,
   Routes,
   SlashCommandBuilder,
   SlashCommandStringOption,
 } from "discord.js";
-import fetch from "node-fetch";
 import auth from "../auth.json" assert { type: "json" };
-import { fetchBingus } from "./util";
+import { EmbedList, fetchBingus } from "./util.js";
 
 const commands = [
   new SlashCommandBuilder()
-    .setName("search")
+    .setName("ask")
     .setDescription("Searches for the answer with Bingus")
     .addStringOption(
       new SlashCommandStringOption()
         .setName("query")
         .setRequired(true)
-        .setDescription("What's your question owo"),
+        .setDescription("What's your question owo")
+        .setMaxLength(200),
     ),
 ];
 
@@ -45,31 +44,71 @@ client.on("ready", () => {
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isCommand()) return;
 
-  if (interaction.commandName === "search") {
+  if (interaction.commandName === "ask") {
     if (!interaction.isChatInputCommand()) return;
-    const query = interaction.options.getString("query") ?? "";
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const query = interaction.options.getString("query")!;
+    console.log(`User @${interaction.user.id} asked about "${query}"`);
 
     try {
-      await interaction.deferReply();
+      await interaction.deferReply({ ephemeral: true });
       const data = await fetchBingus(query);
-      if (data && data.length > 0) {
-        const firstResult = data[0];
-        const embed: APIEmbed = {
-          title: firstResult.title,
-          description: firstResult.text,
-        };
 
-        interaction.editReply({
-          content: "Here is the first result:",
-          embeds: [embed],
-        });
-      } else {
-        interaction.editReply("No results found.");
+      if (data.length === 0) {
+        await interaction.editReply("No results found.");
+        return;
       }
+
+      const embedList = new EmbedList();
+      embedList.push(
+        ...data.map(
+          (res) =>
+            new EmbedBuilder()
+              .setTitle(res.title)
+              .setDescription(res.text)
+              .setColor("#65459A").data,
+        ),
+      );
+
+      await embedList.sendChatInput(interaction);
     } catch (error) {
       console.error(error);
       interaction.editReply("An error occurred while fetching results.");
     }
+  }
+});
+
+// Check if a new thread is created on one of the configured forums to check
+client.on("threadCreate", async (thread, newly) => {
+  if (!newly || !auth.forumsCheck.includes(thread.parentId ?? "")) return;
+
+  try {
+    const data = await fetchBingus(thread.name);
+
+    if (data.length === 0) {
+      return;
+    }
+
+    const embedList = new EmbedList();
+    embedList.push(
+      ...data.map(
+        (res) =>
+          new EmbedBuilder()
+            .setTitle(res.title)
+            .setDescription(res.text)
+            .setColor("#65459A").data,
+      ),
+    );
+
+    await thread.fetch();
+
+    await embedList.sendChannel(
+      thread,
+      thread.ownerId,
+      `Maybe this will help?`,
+    );
+  } catch (error) {
+    console.error(error);
   }
 });
 
--- bingus-bot/src/util.ts
+++ bingus-bot/src/util.ts
@@ -1,4 +1,15 @@
-import { APIEmbed, BaseInteraction, ChatInputCommandInteraction } from "discord.js";
+import {
+  APIEmbed,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ChatInputCommandInteraction,
+  ComponentType,
+  EmbedBuilder,
+  ReplyOptions,
+  TextBasedChannel,
+} from "discord.js";
+import fetch from "node-fetch";
 
 export async function fetchBingus(query: string) {
   const url = `https://bingus.bscotch.ca/api/faq/search?question=${encodeURIComponent(
@@ -20,16 +31,127 @@ export interface BingusFaqResponse {
 }
 
 export class EmbedList {
-  embeds: APIEmbed[] = []
-  index = 0
-  
-  push(embed: APIEmbed): number {
-    return this.embeds.push(embed);
+  static MAX_TIME = 300_000;
+  embeds: APIEmbed[] = [];
+  index = 0;
+
+  push(...embed: APIEmbed[]): number {
+    return this.embeds.push(...embed);
   }
 
-  sendChatInput(interaction: ChatInputCommandInteraction) {
-    return new Promise((res, rej) => {
-      interaction
+  getActionRow(): ActionRowBuilder<ButtonBuilder> {
+    const next = new ButtonBuilder()
+      .setCustomId("next")
+      .setLabel("Next >")
+      .setStyle(ButtonStyle.Secondary)
+      .setDisabled(this.index === this.embeds.length - 1);
+
+    const prev = new ButtonBuilder()
+      .setCustomId("prev")
+      .setLabel("< Prev")
+      .setStyle(ButtonStyle.Secondary)
+      .setDisabled(this.index === 0);
+
+    return new ActionRowBuilder<ButtonBuilder>().addComponents(prev, next);
+  }
+
+  get(): EmbedBuilder {
+    return new EmbedBuilder(this.embeds[this.index]).setFooter({
+      text: `${this.index + 1}/${this.embeds.length}`,
     });
+  }
+
+  async sendChannel(
+    channel: TextBasedChannel,
+    who: string | null,
+    content?: string,
+    reply?: ReplyOptions,
+  ) {
+    const edit = await channel.send({
+      content,
+      embeds: [this.get()],
+      components: [this.getActionRow()],
+      reply,
+    });
+
+    const collector = edit.createMessageComponentCollector({
+      componentType: ComponentType.Button,
+      time: EmbedList.MAX_TIME,
+      filter: who ? (i) => i.user.id === who : undefined,
+    });
+
+    collector.on("collect", async (i) => {
+      switch (i.customId) {
+        case "next":
+          if (this.index === this.embeds.length - 1) {
+            await i.update({});
+            return;
+          }
+          this.index++;
+          break;
+        case "prev":
+          if (this.index === 0) {
+            await i.update({});
+            return;
+          }
+          this.index--;
+      }
+
+      await i.update({
+        embeds: [this.get()],
+        components: [this.getActionRow()],
+      });
+    });
+
+    collector.on("end", async () => {
+      await edit.edit({ components: [] });
+    });
+  }
+
+  async sendChatInput(interaction: ChatInputCommandInteraction) {
+    const reply = await (interaction.deferred
+      ? interaction.editReply({
+          embeds: [this.get()],
+          components: [this.getActionRow()],
+        })
+      : interaction.reply({
+          embeds: [this.get()],
+          components: [this.getActionRow()],
+        }));
+
+    const collector = reply.createMessageComponentCollector({
+      componentType: ComponentType.Button,
+      time: EmbedList.MAX_TIME,
+      filter: (i) => i.user.id === interaction.user.id,
+    });
+
+    collector.on("collect", async (i) => {
+      switch (i.customId) {
+        case "next":
+          if (this.index === this.embeds.length - 1) {
+            await i.update({});
+            return;
+          }
+          this.index++;
+          break;
+        case "prev":
+          if (this.index === 0) {
+            await i.update({});
+            return;
+          }
+          this.index--;
+      }
+
+      await i.update({
+        embeds: [this.get()],
+        components: [this.getActionRow()],
+      });
+    });
+
+    collector.on("end", async () => {
+      await interaction.editReply({ components: [] });
+    });
+
+    return collector;
   }
 }
