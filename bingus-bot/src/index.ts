b45d800519d260bbcc9965b462a0fc408fa7e629
Uriel <urielfontan2002@gmail.com>
2023-07-13 17:41:32 -0300

    add shipping command

--- bingus-bot/src/index.ts
+++ bingus-bot/src/index.ts
@@ -1,47 +1,36 @@
 import {
   AutocompleteInteraction,
+  ChatInputCommandInteraction,
   Client,
   EmbedBuilder,
   GatewayIntentBits,
   REST,
   Routes,
-  SlashCommandBooleanOption,
   SlashCommandBuilder,
-  SlashCommandStringOption,
 } from "discord.js";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import auth from "../auth.json" assert { type: "json" };
-import { EmbedList, fetchBingus, fetchBingusData } from "./util.js";
+import { EmbedList, fetchBingus } from "./util.js";
+import { askCommand } from "./commands/ask.js";
+import { shippingCommand } from "./commands/shipping.js";
 
-const commands = [
-  new SlashCommandBuilder()
-    .setName("ask")
-    .setDescription("Tries to solve your problems >~<")
-    .addStringOption(
-      new SlashCommandStringOption()
-        .setName("query")
-        .setRequired(true)
-        .setDescription("What's your question owo")
-        .setMaxLength(200)
-        .setAutocomplete(true),
-    )
-    .addBooleanOption(
-      new SlashCommandBooleanOption()
-        .setName("visible")
-        .setRequired(false)
-        .setDescription(
-          "Should the message be visible and interactable by others?",
-        ),
-    ),
-];
+export interface Command {
+  builder: Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">;
+  run: (interaction: ChatInputCommandInteraction) => Promise<void>;
+  autocomplete?: (interaction: AutocompleteInteraction) => Promise<void>;
+}
+
+const commands = [askCommand, shippingCommand];
 
 const rest = new REST({ version: "10" }).setToken(auth.token);
 
 try {
   console.log("Started refreshing application (/) commands.");
 
-  await rest.put(Routes.applicationCommands(auth.client), { body: commands });
+  await rest.put(Routes.applicationCommands(auth.client), {
+    body: commands.map((x) => x.builder),
+  });
 
   console.log("Successfully reloaded application (/) commands.");
 } catch (error) {
@@ -51,74 +40,34 @@ try {
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent],
 });
-let faqConfig = (await fetchBingusData()).faqs.flatMap(
-  (x) => x.matched_questions,
-);
-
-setInterval(async () => {
-  faqConfig = (await fetchBingusData()).faqs.flatMap(
-    (x) => x.matched_questions,
-  );
-}, 60 * 60 * 1000); // Do it every hour
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user?.tag}`);
 });
 
-async function onAutocomplete(interaction: AutocompleteInteraction) {
-  if (interaction.commandName === "ask") {
-    const focusedValue = interaction.options.getFocused().toLowerCase();
-    const filtered = faqConfig.filter((x) => x.includes(focusedValue));
-    filtered.length = Math.min(filtered.length, 25);
-    await interaction.respond(filtered.map((x) => ({ name: x, value: x })));
-  }
-}
-
+// Autocomplete handler
 client.on("interactionCreate", async (interaction) => {
-  if (interaction.isAutocomplete()) {
-    return await onAutocomplete(interaction);
-  }
-  if (!interaction.isCommand()) return;
+  if (!interaction.isAutocomplete()) return;
+  const cmd = commands.find(
+    (x) =>
+      x.autocomplete !== undefined &&
+      x.builder.name === interaction.commandName,
+  );
+  if (cmd) await cmd.autocomplete!(interaction);
+});
 
-  if (interaction.commandName === "ask") {
-    if (!interaction.isChatInputCommand()) return;
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const query = interaction.options.getString("query")!;
-    const ephemeral = !interaction.options.getBoolean("visible") ?? true;
-    console.log(`User @${interaction.user.id} asked about "${query}"`);
+// Chat command handler
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
 
-    try {
-      await interaction.deferReply({ ephemeral });
-      const data = await fetchBingus(query);
-
-      if (data.length === 0) {
-        await interaction.editReply("No results found.");
-        return;
-      }
-
-      const embedList = new EmbedList();
-      embedList.push(
-        ...data.map(
-          (res) =>
-            new EmbedBuilder()
-              .setTitle(res.title)
-              .setDescription(res.text)
-              .setColor("#65459A").data,
-        ),
-      );
-
-      await embedList.sendChatInput(interaction);
-    } catch (error) {
-      console.error(error);
-      interaction.editReply("An error occurred while fetching results.");
-    }
-  }
+  const cmd = commands.find((x) => x.builder.name === interaction.commandName);
+  if (cmd) await cmd.run(interaction);
 });
 
 // Check if a new thread is created on one of the configured forums to check
 client.on("threadCreate", async (thread, newly) => {
   if (!newly || !auth.forumsCheck.includes(thread.parentId ?? "")) return;
-  const lastMessage = await thread.fetchStarterMessage()
+  const lastMessage = await thread.fetchStarterMessage();
   if (lastMessage === null) {
     console.error(
       `Couldn't read @${thread.ownerId}'s message on thread #${thread.id}, message ${thread.lastMessageId}`,
