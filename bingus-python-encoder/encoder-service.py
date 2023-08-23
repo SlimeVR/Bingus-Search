f2008523db126e074c703c40fd48040b12c26c2a
Butterscotch! <bscotchvanilla@gmail.com>
2023-08-23 19:50:07 -0400

    Add a basic Python encoding API

---
+++ bingus-python-encoder/encoder-service.py
@@ -0,0 +1,18 @@
+from sentence_transformers import SentenceTransformer
+import json
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+
+class EncodeRequest(BaseModel):
+    sentence: str
+
+model = SentenceTransformer("all-mpnet-base-v2")
+app = FastAPI()
+
+@app.post("/encode/")
+async def encode_sentence(encode_request: EncodeRequest):
+    return json.dumps({"embedding": [val.item() for val in model.encode(encode_request.sentence)]})
+
+if __name__ == "__main__":
+    uvicorn.run("encoder-service:app", host="127.0.0.1", port=5000, log_level="info")
