82fa5aca5ca98c042e9e8269fd3eb181e0ab1a86
Butterscotch! <bscotchvanilla@gmail.com>
2023-09-06 00:42:14 -0400

    Update eval to separate from training

--- bingus-python-encoder/fine-tuner.py
+++ bingus-python-encoder/fine-tuner.py
@@ -43,41 +43,49 @@ sentences2 = []
 scores = []
 
 # Set up eval collection
+include_eval_in_other_pairs = False
 include_eval_in_training = False
-use_defined_count = False
-if use_defined_count:
-    faq_combination_count = (faq_count * faq_count) - faq_count
-    target_eval_count = 1000
-    eval_percent = target_eval_count / faq_combination_count
-else:
-    eval_percent = 0.01
+eval_percent = 0.1
 # Calculated eval params
 eval_interval = math.ceil(1 / eval_percent)
-eval_counter = 0
+exclusive_eval_count = 0
+eval_count = 0
 
 # Collect data pairs
 for faq_set in faqs:
     for faq in faq_set:
+        is_exclusive_eval = not include_eval_in_other_pairs and exclusive_eval_count % eval_interval == 0
+        exclusive_eval_count += 1
+        exclusive_eval_other_count = 0
+
         for other_faq_set in faqs:
             for other_faq in other_faq_set:
+                is_exclusive_eval_other = not include_eval_in_other_pairs and exclusive_eval_other_count % eval_interval == 0
+                exclusive_eval_other_count += 1
+
                 # Of course they're equal if they're the same thing, skip it
-                if faq == other_faq:
+                # Or if it's mixing eval with non-eval, skip it
+                if faq == other_faq or (is_exclusive_eval != is_exclusive_eval_other):
                     continue
+
+                is_eval = include_eval_in_other_pairs and eval_count % eval_interval == 0
+                eval_count += 1
+
                 # If it's part of the same set, it's similar questions, otherwise it's not similar
                 score = 1.0 if faq_set == other_faq_set else 0.0
-                is_eval = eval_counter % eval_interval == 0
 
-                # Add to the training set
-                if not is_eval or include_eval_in_training:
-                    training_set.append(InputExample(
-                        texts=[faq, other_faq], label=score))
+                # Do not include any eval data in training data if it's exclusive
+                if not is_exclusive_eval:
+                    # Add to the training set if not eval
+                    if not is_eval or include_eval_in_training:
+                        training_set.append(InputExample(
+                            texts=[faq, other_faq], label=score))
 
                 # Add to eval if it's within the interval
-                if is_eval:
+                if is_eval or is_exclusive_eval:
                     sentences1.append(faq)
                     sentences2.append(other_faq)
                     scores.append(score)
-                eval_counter += 1
 print(
     f"Generated datasets: \n  > Training: {len(training_set)} entries\n  > Evaluation: {len(sentences1)} entries")
 
@@ -85,7 +93,7 @@ print(
 print("Loading model to fine-tune...")
 model_cache = "./model-cache/"
 model_name = "bingus-fine-tuned"
-model_dir = f"{model_cache}{model_name}/"
+model_dir = f"./local-models/{model_name}/"
 output_path = f"{model_dir}output/"
 checkpoint_path = f"{model_dir}checkpoints/"
 model = SentenceTransformer("all-MiniLM-L6-v2", cache_folder=model_cache)
