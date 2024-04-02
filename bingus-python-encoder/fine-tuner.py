dac188d6b40818373060ff9ede821d0ecfb78520
Butterscotch! <bscotchvanilla@gmail.com>
2024-04-02 03:19:02 -0400

    Add eval mode to fine-tuning script

--- bingus-python-encoder/fine-tuner.py
+++ bingus-python-encoder/fine-tuner.py
@@ -1,6 +1,7 @@
 from sentence_transformers import SentenceTransformer, InputExample, losses, evaluation
 from torch.utils.data import DataLoader
 
+import sys
 import os
 import json
 import math
@@ -8,27 +9,47 @@ import shutil
 
 # Define the default paths to check
 faq_config_paths = ["./faq_config.json",
-                    "../BingusApi/faq_config.json", "./BingusApi/faq_config.json"]
-faq_config_path = None
-faq_config = None
+                    "../BingusApi/config/faq_config.json", "./BingusApi/config/faq_config.json"]
+
+# Eval parameters
+eval_mode = False
+if eval_mode:
+    include_eval_in_other_pairs = False
+    include_eval_in_training = False
+    eval_percent = 0.1
+else:
+    include_eval_in_other_pairs = True
+    include_eval_in_training = True
+    eval_percent = 0
+
+# Model parameters
+model_cache = "./model-cache/"
+base_model = "all-MiniLM-L6-v2"
+
+model_ver = 9
+model_name = f"Bingus-v{model_ver}{'_Eval' if eval_mode else ''}_{base_model}"
+model_dir = f"./local-models/{model_name}/"
+
+output_path = f"{model_dir}output/"
+checkpoint_path = f"{model_dir}checkpoints/"
 
 # Search the paths for the config
+faq_config = None
 for cur_path in faq_config_paths:
     if os.path.isfile(cur_path):
         print(f"Found \"faq_config.json\" at \"{cur_path}\"!")
-        faq_config_path = cur_path
+        print("Reading FAQ config...")
+        with open(cur_path, "r") as f:
+            faq_config = json.load(f)
         break
     else:
         print(f"Could not find \"faq_config.json\" at \"{cur_path}\".")
 
-if faq_config_path is None:
+if faq_config is None:
     raise FileNotFoundError(
         "Could not find \"faq_config.json\" in any of the default paths.")
 
-# Load the config
-print("Reading FAQ config...")
-with open(faq_config_path, "r") as f:
-    faq_config = json.load(f)
+# Parse the config
 faqs = [faq["matched_questions"] for faq in faq_config["faqs"]]
 faq_count = sum(len(faq) for faq in faqs)
 
@@ -42,12 +63,9 @@ sentences1 = []
 sentences2 = []
 scores = []
 
-# Set up eval collection
-include_eval_in_other_pairs = False
-include_eval_in_training = False
-eval_percent = 0.1
 # Calculated eval params
-eval_interval = math.ceil(1 / eval_percent)
+eval_interval = sys.maxsize if eval_percent <= 0 else math.ceil(
+    1 / eval_percent)
 exclusive_eval_count = 0
 eval_count = 0
 
@@ -91,18 +109,13 @@ print(
 
 # Load the model to fine-tune
 print("Loading model to fine-tune...")
-model_cache = "./model-cache/"
-model_name = "bingus-fine-tuned"
-model_dir = f"./local-models/{model_name}/"
-output_path = f"{model_dir}output/"
-checkpoint_path = f"{model_dir}checkpoints/"
-model = SentenceTransformer("all-MiniLM-L6-v2", cache_folder=model_cache)
+model = SentenceTransformer(base_model, cache_folder=model_cache)
 
 # Define the training dataset, the data loader and the training loss
 training_dataloader = DataLoader(training_set, shuffle=True, batch_size=16)
 training_loss = losses.CosineSimilarityLoss(model)
 evaluator = evaluation.EmbeddingSimilarityEvaluator(
-    sentences1=sentences1, sentences2=sentences2, scores=scores)
+    sentences1=sentences1, sentences2=sentences2, scores=scores) if eval_mode else None
 
 # Remove any previously trained outputs
 if os.path.isdir(model_dir):
